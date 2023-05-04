b6014893b18ad60c1892ccc21542e056768e5453
MorZLE <art04.04@mail.ru>
2023-05-04 19:20:49 +0300

    карточки товаров

--- magaz/starmart/urls.py
+++ magaz/starmart/urls.py
@@ -3,7 +3,12 @@ from django.urls import path, include
 from starmart.views import *
 
 urlpatterns = [
-    path('', index),
+    path('', index, name='home'),
     path('cats/<slug:cat>', categories),
-    path('goods/', goods),
+    path('goods/', goods, name='goods'),
+    path('about/', about, name='about'),
+    path('basket/', basket, name='basket'),
+    path('login/', login, name='login'),
+    path('good/<int:good>/', show_good, name='good')
+
 ]
--- magaz/starmart/views.py
+++ magaz/starmart/views.py
@@ -5,7 +5,8 @@ from .models import *
 
 # Create your views here.
 
-menu = ['Товары', 'О нас', 'Корзина', 'Войти']
+menu = [{'title': 'Товары', 'url_name': 'goods'}, {'title': 'О нас', 'url_name': 'about'},
+        {'title': 'Корзина', 'url_name': 'basket'}, {'title': 'Войти', 'url_name': 'login'}]
 
 
 def index(request):
@@ -14,11 +15,54 @@ def index(request):
 
 
 def goods(request):
-    return HttpResponse('Все товары')
+    good = Goods.objects.all()
+    cat = Categories.objects.all()
+    context = {
+        'menu': menu,
+        'title': 'Товары',
+        'goods': good,
+        'cat': cat
+    }
+    return render(request, 'starmart/goods.html', context=context)
+
+
+def show_good(request, good):
+    good = Goods.pk
+
+    context = {
+        'menu': menu,
+        'title': 'Товары',
+        'goods': good
+    }
+    return render(request, 'starmart/good.html', context=context)
 
 
 def categories(request, cat):
-    return HttpResponse(f'категория  {cat}')
+    return HttpResponse(f'Категория {cat}')
+
+
+def about(request):
+    context = {
+        'menu': menu,
+        'title': 'О нас',
+    }
+    return render(request, 'starmart/about.html', context=context)
+
+
+def basket(request):
+    context = {
+        'menu': menu,
+        'title': 'Корзина'
+    }
+    return render(request, 'starmart/basket.html', context=context)
+
+
+def login(request):
+    context = {
+        'menu': menu,
+        'title': 'Авторизация'
+    }
+    return render(request, 'starmart/login.html', context=context)
 
 
 def pageNotFound(request, exception):
