99d13f68c3d45f06554036227271ba50ec2af134
MorZLE <art04.04@mail.ru>
2023-06-01 11:24:26 +0300

    классы представлений

--- magaz/starmart/urls.py
+++ magaz/starmart/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 
 from starmart.views import *
 
+
 urlpatterns = [
-    path('', index, name='home'),
-    path('goods/', goods, name='goods'),
+    path('', ShopHome.as_view(), name='home'),
+    path('goods/', ShopGoods.as_view(), name='goods'),
     path('about/', about, name='about'),
     path('basket/', basket, name='basket'),
     path('login/', login, name='login'),
-    path('good/<int:good>/', show_good, name='good'),
-    path('cat/<int:cat>', show_category, name='cat')
+    path('good/<int:good>/', ShowGood.as_view(), name='good'),
+    path('cat/<int:cat>', ShowCategory.as_view(), name='cat')
 ]
--- magaz/starmart/views.py
+++ magaz/starmart/views.py
@@ -1,58 +1,68 @@
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 from django.shortcuts import HttpResponse
-from .models import *
+from django.views.generic import ListView, DetailView
 
+from .models import *
 
 
 menu = [{'title': 'Товары', 'url_name': 'goods'}, {'title': 'О нас', 'url_name': 'about'},
         {'title': 'Корзина', 'url_name': 'basket'}, {'title': 'Войти', 'url_name': 'login'}]
 
 
-def index(request):
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'Главная',
-        'cat': cat
-    }
-    return render(request, 'starmart/index.html', context=context)
+class ShopHome(ListView):
+    model = Goods
+    context_object_name = 'goods'
+    template_name = 'starmart/index.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['cat'] = Categories.objects.all()
+        context['title'] = 'Главная'
+        return context
 
 
-def goods(request):
+class ShopGoods(ListView):
+    model = Goods
+    context_object_name = 'goods'
+    template_name = 'starmart/goods.html'
     good = Goods.objects.filter(is_published=True)
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'Товары',
-        'goods': good,
-        'cat': cat
-    }
-    return render(request, 'starmart/goods.html', context=context)
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['cat'] = Categories.objects.all()
+        context['title'] = 'Товары'
+        context['goods'] = self.good
+        return context
+
+    def get_queryset(self):
+        return Goods.objects.filter(is_published=True)
+
+class ShowGood(DetailView):
+    model = Goods
+    template_name = 'starmart/good.html'
+    pk_url_kwarg = 'good'
+    context_object_name = 'good'
 
 
+class ShowCategory(ListView):
+    model = Goods
+    context_object_name = 'goods'
+    template_name = 'starmart/goods.html'
 
-def show_good(request, good):
-    good = Goods.objects.filter(pk=good, is_published=True)
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'good': good,
-        'cat': cat
-    }
-    return render(request, 'starmart/good.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['cat'] = Categories.objects.all()
+        context['title'] = 'Товары'
+        return context
+
+    def get_queryset(self):
+        return Goods.objects.filter(category_id=self.kwargs['cat'], is_published=True)
 
 
-def show_category(request, cat):
-    good = Goods.objects.filter(category_id=cat, is_published=True)
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'Товары',
-        'goods': good,
-        'cat': cat
-    }
-    return render(request, 'starmart/goods.html', context=context)
 
 
 def about(request):
