f3a0d831f303541ac78d67a8977d0286f5be9718
MorZLE <art04.04@mail.ru>
2023-06-02 22:49:24 +0300

    классы представлений 1

--- magaz/starmart/urls.py
+++ magaz/starmart/urls.py
@@ -6,9 +6,10 @@ from starmart.views import *
 urlpatterns = [
     path('', ShopHome.as_view(), name='home'),
     path('goods/', ShopGoods.as_view(), name='goods'),
-    path('about/', about, name='about'),
-    path('basket/', basket, name='basket'),
-    path('login/', login, name='login'),
+    path('about/', ShopAbout.as_view(), name='about'),
+    path('basket/', ShopBasket.as_view(), name='basket'),
+    path('login/', ShopLogin.as_view(), name='login'),
+    path('adminka/', ShopAdmin.as_view(), name='admin'),
     path('good/<int:good>/', ShowGood.as_view(), name='good'),
     path('cat/<int:cat>', ShowCategory.as_view(), name='cat')
 ]
--- magaz/starmart/views.py
+++ magaz/starmart/views.py
@@ -1,98 +1,116 @@
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView
 
+
 from .models import *
+from .utils import *
 
 
-menu = [{'title': 'Товары', 'url_name': 'goods'}, {'title': 'О нас', 'url_name': 'about'},
-        {'title': 'Корзина', 'url_name': 'basket'}, {'title': 'Войти', 'url_name': 'login'}]
-
-
-class ShopHome(ListView):
+class ShopHome(DataMixin, ListView):
     model = Goods
     context_object_name = 'goods'
     template_name = 'starmart/index.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['cat'] = Categories.objects.all()
-        context['title'] = 'Главная'
-        return context
+        c_def = self.get_user_context(title='Главная')
+        return dict(list(context.items())+list(c_def.items()))
 
 
-class ShopGoods(ListView):
+class ShopGoods(DataMixin, ListView):
     model = Goods
     context_object_name = 'goods'
     template_name = 'starmart/goods.html'
-    good = Goods.objects.filter(is_published=True)
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['cat'] = Categories.objects.all()
-        context['title'] = 'Товары'
-        context['goods'] = self.good
-        return context
+        c_def = self.get_user_context(title='Товары')
+        return dict(list(context.items()) + list(c_def.items()))
 
     def get_queryset(self):
         return Goods.objects.filter(is_published=True)
 
-class ShowGood(DetailView):
+class ShowGood(DataMixin, DetailView):
     model = Goods
     template_name = 'starmart/good.html'
     pk_url_kwarg = 'good'
     context_object_name = 'good'
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Товары')
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-class ShowCategory(ListView):
+class ShowCategory(DataMixin, ListView):
     model = Goods
     context_object_name = 'goods'
     template_name = 'starmart/goods.html'
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['menu'] = menu
-        context['cat'] = Categories.objects.all()
-        context['title'] = 'Товары'
-        return context
+        c_def = self.get_user_context(title='Товары')
+        return dict(list(context.items())+list(c_def.items()))
 
     def get_queryset(self):
         return Goods.objects.filter(category_id=self.kwargs['cat'], is_published=True)
 
 
+class ShopAdmin(LoginRequiredMixin, DataMixin, ListView):
+    model = Goods
+    context_object_name = 'goods'
+    template_name = 'starmart/admin.html'
+  #  raise_exception = True
+   # login_url = reverse_lazy('login')
+    login_url = '/admin'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Админка")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        pass
 
 
-def about(request):
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'О нас',
-        'cat': cat
-    }
-    return render(request, 'starmart/about.html', context=context)
+class ShopAbout(DataMixin, ListView):
+    template_name = 'starmart/about.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="О нас")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        pass
 
 
-def basket(request):
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'Корзина',
-        'cat': cat
-    }
-    return render(request, 'starmart/basket.html', context=context)
 
+class ShopBasket(DataMixin, ListView):
+    template_name = 'starmart/basket.html'
 
-def login(request):
-    cat = Categories.objects.all()
-    context = {
-        'menu': menu,
-        'title': 'Авторизация',
-        'cat': cat
-    }
-    return render(request, 'starmart/login.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Корзина")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        pass
+
+class ShopLogin(DataMixin, ListView):
+    template_name = 'starmart/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Вход")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_queryset(self):
+        pass
+
 
 
 def pageNotFound(request, exception):
