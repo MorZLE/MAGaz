94db32b5a0e8cc53ee98452fd6bc3828ec28a6b5
MorZLE <art04.04@mail.ru>
2023-06-06 13:38:14 +0300

    вкладка база

--- magaz/starmart/utils.py
+++ magaz/starmart/utils.py
@@ -3,8 +3,7 @@ from django.db.models import Count
 from starmart.models import Categories
 
 menu = [{'title': 'Товары', 'url_name': 'goods'}, {'title': 'О нас', 'url_name': 'about'},
-        {'title': 'Корзина', 'url_name': 'basket'},
-        {'title': 'Админка', 'url_name': 'admin'}]
+        {'title': 'Корзина', 'url_name': 'basket'}]
 
 
 class DataMixin:
@@ -13,12 +12,6 @@ class DataMixin:
         context = kwargs
         cats = Categories.objects.annotate(Count('goods'))
         user_menu = menu.copy()
-
-        if not self.request.user.is_superuser:
-            user_menu.pop(3)
-
-
-
         context['menu'] = user_menu
         context['cat'] = cats
         return context
